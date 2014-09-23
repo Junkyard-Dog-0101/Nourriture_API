7a64f41eb3e9d566db1f2abe744c739465fab7fc
Julien Vesy <julien.vesy@gmail.com>
2014-09-23 06:26:10 -0700

    Revert "norme"
    This reverts commit 6efe47749fec9cf19c1a40bd5ebfb52a035b8b1e.

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var recipes = require('./routes/recipes');
-var mongoose = require('mongoose');
-
 var app = express();
-var port = process.env.PORT || 8080; 
-
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use('/api/recipes', recipes);
+var port = process.env.PORT || 8080; 
 
+var mongoose = require('mongoose');
 mongoose.connect('127.0.0.1:1234');
+
+var recipes = require('./routes/recipes');
+
+
+app.use('/api/recipes', recipes);
 
 app.listen(port);
