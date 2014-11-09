209cb5340039c18c47377b0f19a0069f9be51c61
Julien Vesy <julien.vesy@gmail.com>
2014-11-09 01:43:32 -0800

    begginning implement login

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var User = require('../app/models/user');
+var passport = require('passport');
 var router = express.Router();
 
 
 router.route('/')
     .post(function (req, res) {
         var user = new User();
-        user.login = req.body.login;
+        user.username = req.body.username;
         user.password = req.body.password;
         user.save(function (err) {
             if (err)
@@ -21,6 +22,37 @@ router.route('/')
             res.json(users);
         });
     });
+
+router.route('/register')
+    .get(function(req, res) {
+	res.render('register', { });
+    })
+    .post(function (req, res) {
+	User.register(new User({ username : req.body.username }), req.body.password, function(err, account) {
+            if (err) {
+		return res.send(err);//render("register", {info: "Sorry. That username already exists. Try again."});
+            }
+	    
+            passport.authenticate('local')(req, res, function () {
+		res.json({message: 'User created and logged in'});
+            });
+	});
+    });
+router.route('/login')
+    .get(function(req, res) {
+	res.json({ message : req.user });
+    })
+
+    .post(passport.authenticate('local'), function(req, res) {
+	res.json({message: 'User logged in'});
+    });
+
+router.route('/logout')
+    .get(function(req, res) {
+	req.logout();
+	res.json({message: 'User logged out'});
+    });
+
 
 router.route('/:user_id')
     .get(function (req, res) {
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 
 var recipes = require('./routes/recipes');
 var ingredients = require('./routes/ingredients');
@@ -12,6 +14,13 @@ var port = process.env.PORT || 8080;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(passport.initialize());
+app.use(passport.session());
+
+var User = require('./app/models/user');
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.use('/api/recipes', recipes);
 app.use('/api/ingredients', ingredients);
