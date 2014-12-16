ce3d8dab9dbdc7b37bb56bd96fb3003299d531c7
julien <julien.vesy@gmail.com>
2014-12-17 00:59:28 +0800

    modifying friend

--- controllers/friend.js
+++ controllers/friend.js
@@ -1,9 +1,9 @@
 var Friend = require('../models/friend');
 
-exports.postFriends = function (req, res) {
+exports.sendFriendRequest = function (req, res) {
   var friend = new Friend();
   friend.content = req.body.content;
-  friend.from = req.body.from;
+  friend.from = req.user._id;
   friend.to = req.body.to;
   friend.save(function (err) {
     if (err)
@@ -20,12 +20,12 @@ exports.validateFriendRequest = function (req, res) {
     else if (!friend)
       res.status(404).end();
     else
-      res.status(200).json(friend);
+      res.status(204);
     });
 };
 
 exports.getMyFriendRequest = function (req, res) {
-  Friend.find({to: req.params.friend_id, status: "accept"}, function (err, friend) {
+  Friend.find({to: req.user._id, status: "request"}, function (err, friend) {
     if (err)
       res.status(400).json(err);
     else if (!friend)
@@ -39,7 +39,7 @@ exports.getMyFriend = function (req, res) {
   Friend.find({ $and: [
     { to: req.user._id, status: "accept" },
     { from: req.user._id, status: "accept" }
-  ]}, function (err, friend) {
+  ] }, function (err, friend) {
     if (err)
       res.status(400).json(err);
     else if (!friend)
@@ -49,11 +49,25 @@ exports.getMyFriend = function (req, res) {
   });
 };
 
-/*exports.deleteFriend = function (req, res) {
-    Friend.remove({from: req.body.friend_id}, function (err) {
-        if (err)
-            res.status(400).json(err);
-        else
-            res.status(204).end();
-    });
-};*/
+exports.deleteFriend = function (req, res) {
+  Friend.remove({ $and: [
+    { to: req.user._id, from: req.body.friend_id, status: "accept" },
+    { from: req.user._id, to: req.body.friend_id,status: "accept" }
+  ] }, function (err) {
+    if (err)
+      res.status(400).json(err);
+    else
+      res.status(204).end();
+  });
+};
+
+/*
+*exports.deleteFriendRequest = function (req, res) {
+*  Friend.remove({ from: req.body.friend_id }, function (err) {
+*    if (err)
+*      res.status(400).json(err);
+*    else
+*      res.status(204).end();
+*  });
+*};
+*/
