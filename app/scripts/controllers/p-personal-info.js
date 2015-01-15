2fe70ad9af6d0a77e013043f98d424a0eb3b6a91
Joncer <229823811@qq.com>
2015-01-15 22:48:28 +0800

    解决冲突

--- app/scripts/controllers/p-personal-info.js
+++ app/scripts/controllers/p-personal-info.js
@@ -8,10 +8,34 @@
  * Controller of the webNourritureApp
  */
 angular.module('webNourritureApp')
-  .controller('PPersonalInfoCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('PPersonalInfoCtrl', function ($rootScope,$scope,$http,$window) {
+  	if ($rootScope.globals.currentUser) {
+  		$scope.username=$window.sessionStorage['username'];
+ 	 	$scope.userid=$window.sessionStorage['userid'];
+ 	 	$http.get('api/users/'+$scope.userid).success(function  (data) {
+ 	 		$scope.currentUser=data[0];
+ 	 	});
+  	}; 
+
+  	$scope.modify=function  () {
+	 	$http({
+	                method: 'PUT',
+	                url: 'api/users/'+$scope.userid,
+	                data:  "username="+$scope.currentUser.username+
+	                "&first_name="+$scope.currentUser.firstName+
+	                "&last_name="+$scope.currentUser.lastName+
+	                "&gender="+$scope.currentUser.gender+
+	                "&email="+$scope.currentUser.email,
+	                headers:{
+	                    'Content-Type': 'application/x-www-form-urlencoded'
+	                }
+	            }).success(function  (data,status,headers,config) {
+	                $scope.message='register success';
+	                $window.location.reload();
+	            })
+	            .error(function (data, status, headers, config) {
+	                $scope.message='register error';
+	            });
+  	} ; 
+
   });
