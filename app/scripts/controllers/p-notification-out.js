4487505000b0a8b1495f61383cf4add1c5a99426
凌硕 <lingshuo1993@hotmail.com>
2015-01-18 15:01:42 +0800

    notification

--- app/scripts/controllers/p-notification-out.js
+++ app/scripts/controllers/p-notification-out.js
@@ -8,6 +8,9 @@
  * Controller of the webNourritureApp
  */
 angular.module('webNourritureApp')
-  .controller('PNotificationOutCtrl', function ($scope) {
-
+  .controller('PNotificationOutCtrl', function ($scope,$http,$window) {
+	$http.get('api/getMyNotifications/').success(function  (data) {
+            		$scope.notificationlist=data;
+            		$scope.number=$scope.notificationlist.length;
+          	}).error();
   });
